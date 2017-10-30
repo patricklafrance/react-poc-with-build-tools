cb10935b7e11e40dc30b4aa9efc45408abdbd13c
patrick.lafrance <patrick.lafrance@outlook.com>
2017-10-30 13:05:41 -0400

    Fix toggle element

--- src/component.toggle.js
+++ src/component.toggle.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import PropTypes from "prop-types";
+import _ from "lodash";
 
 export class Toggle extends Component {
     static propTypes = {
@@ -16,12 +17,14 @@ export class Toggle extends Component {
 
     _toggleElement = null;
 
-    componentDidMount() {
-        this._toggleElement.checkbox();
-    }
-    
+    // componentDidMount() {
+    //     this._toggleElement.checkbox();
+    // }
+
     componentWillUnmount() {
-        this._toggleElement.checkbox("destroy");
+        if (!_.isNil(this._toggleElement)) {
+            this._toggleElement.checkbox("destroy");
+        }
     }
 
     handleChange = event => {
@@ -33,6 +36,7 @@ export class Toggle extends Component {
     handleToggleRef = element => {
         // eslint-disable-next-line
         this._toggleElement = $(element);
+        this._toggleElement.checkbox();
     };
 
     render() {
@@ -40,10 +44,10 @@ export class Toggle extends Component {
 
         return (
             <div className="ui toggle checkbox">
-                <input type="checkbox" 
+                <input type="checkbox"
                        name={name}
                        defaultChecked={isChecked}
-                       onChange={this.handleChange} 
+                       onChange={this.handleChange}
                        ref={this.handleToggleRef} />
                 <label></label>
             </div>
